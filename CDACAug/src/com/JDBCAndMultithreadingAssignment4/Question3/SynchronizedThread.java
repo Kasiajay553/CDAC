b5a575527706db6db84b2b2f46935cf450443a0e
kasiajay553 <tkasiajay@gmail.com>
2026-09-14 21:20:28 +0530

    add the question 3 in multithreading using synchronization

---
+++ CDACAug/src/com/JDBCAndMultithreadingAssignment4/Question3/SynchronizedThread.java
@@ -0,0 +1,10 @@
+package com.JDBCAndMultithreadingAssignment4.Question3;
+
+public class SynchronizedThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
